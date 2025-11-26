f0fac50a42b4d35287635f34c84f90b6102feeb8
NicoCisternas1111 <ni.cisternasl@duocuc.cl>
2025-11-26 19:05:25 -0300

    Test y apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,13 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // 🔹 TEST UNITARIOS
+    testImplementation("junit:junit:4.13.2")
+
+    // 🔹 TESTS INSTRUMENTADOS (por si acaso)
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
